03da788a865d6ff575a376e7f549af577dcf2007
Gabriel <gabrielsrosas@gmail.com>
2022-11-21 01:29:45 -0300

    ...

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from app.app import show_predict_page, show_explore_page
+from app import show_predict_page, show_explore_page
 
 page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 
